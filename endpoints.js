afc3fe768f445c34ee35a2e5f1d7df1d8c28c559
Painman-Ren <rds.98.videos@gmail.com>
2022-06-01 21:19:12 -0500

    Test

--- endpoints.js
+++ endpoints.js
@@ -9,7 +9,7 @@ const ajv = new Ajv({coerceTypes:true});
 //db
 const Firestore = require('@google-cloud/firestore')
 const db =  new Firestore()
-const ordered_entriesRef=db.collection('entries').orderBy(`additiondate`, 'desc').orderBy(`additiondate`, 'asc');
+const ordered_entriesRef=db.collection('entries').orderBy(`additiondate`, 'desc').orderBy(`title`, 'asc');
 //Add validation schemas to the validator or future use.
 ajv.addSchema(
     {
